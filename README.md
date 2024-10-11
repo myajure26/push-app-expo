2721fc36e30bb9fe0e1c3847264279bb97ebbb52
Mariangel Yajure <mariangel.yajure@gmail.com>
2024-10-11 14:35:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PushApp
+
+Si quieren que funcione en sus dispositivos con la redirección al tocar la notificación, deben de hacer el prebuild y la configuración de Google Services.
+
+## Documentación
+
+- [Setup Push Notifications](https://docs.expo.dev/push-notifications/push-notifications-setup/)
+- [Setup FCM](https://docs.expo.dev/push-notifications/fcm-credentials/)
---
+++ app/_layout.tsx
@@ -0,0 +1,55 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({}, 'background');
+
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backgroundColor,
+          },
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Push App',
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
+}
